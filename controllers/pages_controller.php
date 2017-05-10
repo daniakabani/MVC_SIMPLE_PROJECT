2136d1d7bfd5683946598f6ad6a503e81fd9b2bf
Dani Alakabani <dannyakabany@icloud.com>
2017-05-10 18:35:58 +0800

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,8 +1,8 @@
 <?php
   class PagesController {
     public function home() {
-      $first_name = 'Jon';
-      $last_name  = 'Snow';
+      $first_name = 'Dani';
+      $last_name  = 'Alakabani';
       require_once('views/pages/home.php');
     }
 
